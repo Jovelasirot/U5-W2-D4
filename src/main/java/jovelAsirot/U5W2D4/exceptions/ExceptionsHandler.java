08629b37418b16e0e28110788d37bc9325d6138a
Jovelasirot <jovelasirot@gmail.com>
2024-04-11 17:34:46 +0200

    validations fixed

--- src/main/java/jovelAsirot/U5W2D4/exceptions/ExceptionsHandler.java
+++ src/main/java/jovelAsirot/U5W2D4/exceptions/ExceptionsHandler.java
@@ -1,27 +1,36 @@
 package jovelAsirot.U5W2D4.exceptions;
 
-import org.apache.coyote.BadRequestException;
+import jovelAsirot.U5W2D4.payloads.ErrorResponseDTO;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.time.LocalDateTime;
+import java.util.stream.Collectors;
 
 @RestControllerAdvice
 public class ExceptionsHandler {
 
-    @ExceptionHandler(BadRequestException.class)
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ErrorsPayLoad handleBadRequest(BadRequestException ex) {
-        return new ErrorsPayLoad(ex.getMessage(), LocalDateTime.now());
-    }
-
     @ExceptionHandler(NotFoundException.class)
-    @ResponseStatus(HttpStatus.NOT_FOUND) // 404
+    @ResponseStatus(HttpStatus.NOT_FOUND)
 
     public ErrorsPayLoad handleNotFound(NotFoundException ex) {
         return new ErrorsPayLoad(ex.getMessage(), LocalDateTime.now());
+    }
+
+    @ExceptionHandler(BadRequestException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST) // 400
+    public ErrorResponseDTO handleBadRequest(BadRequestException ex){
+        if(ex.getErrorsList() != null) {
+            String message = ex.getErrorsList().stream()
+                    .map(objectError -> objectError.getDefaultMessage())
+                    .collect(Collectors.joining(". " ));
+            return new ErrorResponseDTO(message, LocalDateTime.now());
+
+        } else {
+            return new ErrorResponseDTO(ex.getMessage(), LocalDateTime.now());
+        }
     }
 
     @ExceptionHandler(Exception.class)
